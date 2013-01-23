4b720685a8939c045df49be7f1e1df9479909678
Will Fairclough <wfairclough@gmail.com>
2013-01-23 10:34:16 -0500

    Initial Commit

---
+++ BankAccount/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BankAccount
+//
+//  Created by Will Fairclough on 2013-01-23.
+//  Copyright (c) 2013 Will Fairclough. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
