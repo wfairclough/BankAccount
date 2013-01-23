8fa6f4915577aeb07b496f420f94b7f7bb75af87
Will Fairclough <wfairclough@gmail.com>
2013-01-23 10:51:47 -0500

    Added Unit Test Framework to project for later testing.

---
+++ UnitTests/UnitTests.h
@@ -0,0 +1,13 @@
+//
+//  UnitTests.h
+//  UnitTests
+//
+//  Created by Will Fairclough on 2013-01-23.
+//  Copyright (c) 2013 Will Fairclough. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests : SenTestCase
+
+@end
---
+++ UnitTests/UnitTests.m
@@ -0,0 +1,32 @@
+//
+//  UnitTests.m
+//  UnitTests
+//
+//  Created by Will Fairclough on 2013-01-23.
+//  Copyright (c) 2013 Will Fairclough. All rights reserved.
+//
+
+#import "UnitTests.h"
+
+@implementation UnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTests");
+}
+
+@end
